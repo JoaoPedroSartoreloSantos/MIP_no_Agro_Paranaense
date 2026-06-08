b0cc1adab3523b8214672e3234c8965301a3931d
João Pedro Sartorelo Santos <joao.sartorelo.santos@escola.pr.gov.br>
2026-06-08 11:06:41 -0300

    Finalização

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    // Pega a tag <html> que é a raiz do site
+
     const htmlRoot = document.documentElement; 
-    let tamanhoFonteBase = 16; // Tamanho original em pixels
+    let tamanhoFonteBase = 16;
 
     const btnAumentar = document.getElementById("btn-aumentar-zoom");
     const btnDiminuir = document.getElementById("btn-diminuir-zoom");
@@ -12,18 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
         return;
     }
 
-    // Evento para Aumentar
+  
     btnAumentar.addEventListener("click", function() {
         if (tamanhoFonteBase < 26) { 
-            tamanhoFonteBase += 2; // Aumenta 2px na raiz
+            tamanhoFonteBase += 2; 
             htmlRoot.style.fontSize = tamanhoFonteBase + "px";
         }
     });
 
-    // Evento para Diminuir
+    
     btnDiminuir.addEventListener("click", function() {
         if (tamanhoFonteBase > 12) { 
-            tamanhoFonteBase -= 2; // Diminui 2px na raiz
+            tamanhoFonteBase -= 2;  
             htmlRoot.style.fontSize = tamanhoFonteBase + "px";
         }
     });
